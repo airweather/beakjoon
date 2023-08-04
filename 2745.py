7c0b80bf313046885b38bb212d314774d1985a77
Jun <wnsghd1@naver.com>
2023-08-04 23:29:39 +0900

    2745

---
+++ 2745.py
@@ -0,0 +1,24 @@
+decimal = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+number, base = input().split()
+base = int(base)
+
+convertedNumber = 0
+
+for idx, num in enumerate(reversed(number)):
+    
+    
+    if(num in decimal):
+        # num = int(num) * (base**idx)
+        # if(num > base):
+        #     break
+        # else: convertedNumber += num
+        convertedNumber += int(num) * (base**idx)
+    else:
+        # num = (ord(num) - 55) * (base**idx)
+        # if(num > base):
+        #     break
+        # else: convertedNumber += num
+        convertedNumber += (ord(num) - 55) * (base**idx)
+
+print(convertedNumber)
