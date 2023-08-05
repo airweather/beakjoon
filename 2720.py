34b3bd0da7d6b6d536fb59757decd64c576dc29a
J <j@Jui-MacBookPro.local>
2023-08-05 21:07:37 +0900

    2720

---
+++ 2720.py
@@ -0,0 +1,15 @@
+change = [25, 10, 5, 1]
+
+# 첫번째 for 문
+# test case 수
+
+# 안쪽 for 문
+# 4번
+
+for i in range(int(input())):
+    changeList = []
+    money = int(input())
+    for j in range(4):
+        changeList.append(str(money // change[j]))
+        money = money % change[j]
+    print(' '.join(changeList))
