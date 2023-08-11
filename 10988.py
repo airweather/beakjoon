44cbb4665544f79bdaa8066992f73e5aaf1f822a
Jun <wnsghd1@naver.com>
2023-08-12 01:48:23 +0900

    10988

--- 10988.py
+++ 10988.py
@@ -1,8 +1,18 @@
-text = input()
-flag = 1
+# text = input()
+# flag = 1
 
-for i in range(len(text)//2):
-    if(text[i] != text[len(text)-i-1]):
-        flag = 0
+# for i in range(len(text)//2):
+#     if(text[i] != text[len(text)-i-1]):
+#         flag = 0
         
-print(flag)
+# print(flag)
+
+text = input()
+
+if text == text[::-1]:
+    print(1)
+else:
+    print(0)
+    
+# [start:end:step]
+# step이 -1일 경우 역순
