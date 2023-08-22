92104da95604f2066bfd14052f5aac2ae1eafb59
Jun <wnsghd1@naver.com>
2023-08-23 00:42:03 +0900

    10811

---
+++ 10811.py
@@ -0,0 +1,14 @@
+n, m = map(int, input().split())
+
+basket = [i+1 for i in range(n)]
+
+for _ in range(m):
+    i, j = map(int, input().split())
+    
+    while i < j:
+        basket[i-1], basket[j-1] = basket[j-1], basket[i-1]
+        
+        i = i + 1
+        j = j - 1
+        
+print(" ".join(map(str, basket)))
