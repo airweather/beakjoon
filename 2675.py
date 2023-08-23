dbbae5dc5b4da8ca788ce82559f6e388854917b3
Jun <wnsghd1@naver.com>
2023-08-24 00:04:26 +0900

    2675

--- 2675.py
+++ 2675.py
@@ -1,7 +1,14 @@
+# t = int(input())
+
+# for _ in range(t):
+#     r, s = input().split()
+#     for i in range(len(s)):
+#         print(s[i]*int(r), end="")
+#     print()
+
 t = int(input())
 
 for _ in range(t):
     r, s = input().split()
-    for i in range(len(s)):
-        print(s[i]*int(r), end="")
-    print()
+    result = ''.join([char * int(r) for char in s])
+    print(result)
