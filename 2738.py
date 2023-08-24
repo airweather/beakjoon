ae85f822579a7add283328adf0c6b45a00938ae9
Jun <wnsghd1@naver.com>
2023-08-25 01:19:05 +0900

    2738

--- 2738.py
+++ 2738.py
@@ -1,17 +1,27 @@
+# n, m = map(int, input().split())
+
+# firstMatrix = list()
+# secondMatrix = list()
+
+# sumMatrix = list()
+
+# for _ in range(n):
+#     firstMatrix.append(input().split())
+
+# for _ in range(n):
+#     secondMatrix.append(input().split())
+
+# for i in range(n):
+#     for j in range(m):
+#         print(int(firstMatrix[i][j]) + int(secondMatrix[i][j]), end=" ")
+#     print()
+
 n, m = map(int, input().split())
 
-firstMatrix = list()
-secondMatrix = list()
+first_matrix = [list(map(int, input().split())) for _ in range(n)]
 
-sumMatrix = list()
-
-for _ in range(n):
-    firstMatrix.append(input().split())
-
-for _ in range(n):
-    secondMatrix.append(input().split())
+second_matrix = [list(map(int, input().split())) for _ in range(n)]
 
 for i in range(n):
-    for j in range(m):
-        print(int(firstMatrix[i][j]) + int(secondMatrix[i][j]), end=" ")
-    print()
+    result_row = [str(first_matrix[i][j] + second_matrix[i][j]) for j in range(m)]
+    print(" ".join(result_row))
