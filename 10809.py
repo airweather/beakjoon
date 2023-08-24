e47c63aaa15c637b09915cf6d15d4562db4611b6
Jun <wnsghd1@naver.com>
2023-08-25 01:51:02 +0900

    10809

--- 10809.py
+++ 10809.py
@@ -1,8 +1,19 @@
-alphabet = [-1] * 26
+# alphabet = [-1] * 26
+# text = input()
+
+# for idx, char in enumerate(text):
+#     if alphabet[(ord(char)-97)] == -1:
+#         alphabet[(ord(char)-97)] = idx
+
+# print(" ".join(map(str, alphabet)))
+
+alphabet = {}
 text = input()
 
 for idx, char in enumerate(text):
-    if alphabet[(ord(char)-97)] == -1:
-        alphabet[(ord(char)-97)] = idx
+    if char not in alphabet:
+        alphabet[char] = idx
+    print(alphabet)
 
-print(" ".join(map(str, alphabet)))
+result = [alphabet.get(chr(97 + i), -1) for i in range(26)]
+print(" ".join(map(str, result)))
