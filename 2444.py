41613c594b2e16d9be14dcf7ad4b518651a3281e
Jun <wnsghd1@naver.com>
2023-08-31 00:33:38 +0900

    2444

---
+++ 2444.py
@@ -0,0 +1,17 @@
+num = int(input())
+
+for i in range(num):
+    for j in range(num + i):
+        if j < num - 1 - i:
+            print(" ", end="")
+        else:
+            print("*", end="")
+    print()
+
+for i in range(num - 1):
+    for j in range((num-1) * 2 - i):
+        if j <= i:
+            print(" ", end="")
+        else:
+            print("*", end="")
+    print()
