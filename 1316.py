8b99d7f35363126173d2db083ca8ebfc23b227e8
Jun <wnsghd1@naver.com>
2023-10-29 00:32:48 +0900

    1316

--- 1316.py
+++ 1316.py
@@ -1,21 +1,36 @@
+# count = 0
+
+# for _ in range(int(input())):
+#     alphabet = set()
+
+#     lastWord = ""
+    
+#     for word in input():
+#         if(lastWord == word):
+#             lastWord = word
+#             continue
+#         else:
+#             if word in alphabet:
+#                 break
+#             else:
+#                 alphabet.add(word)
+#                 lastWord = word
+#     else:
+#         count += 1
+        
+# print(count)
+
 count = 0
 
 for _ in range(int(input())):
-    alphabet = set()
-
-    lastWord = ""
+    unique_chars = set()
+    word = input()
     
-    for word in input():
-        if(lastWord == word):
-            lastWord = word
-            continue
-        else:
-            if word in alphabet:
-                break
-            else:
-                alphabet.add(word)
-                lastWord = word
+    for i, char in enumerate(word):
+        if i > 0 and char != word[i - 1] and char in unique_chars:
+            break
+        unique_chars.add(char)
     else:
         count += 1
-        
+
 print(count)
